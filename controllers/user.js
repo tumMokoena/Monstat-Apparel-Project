db4246ce87a3e60e0e5d625ce8c389128946cc79
Tumelo Mokoena <mokoena532@gmail.com>
2020-08-28 00:08:43 +0200

    done templating for all available html pages

--- controllers/user.js
+++ controllers/user.js
@@ -1,9 +1,27 @@
-const app = require('../server');
-const express = require('express');
-
 //@desc     show featured products in index page
 //@route    get  /shop/v1/
 //@access   public
 exports.index = (req, res, next) => {
   res.render('index');
+};
+
+//@desc     show app products in the selected category
+//@route    get  /shop/v1/category/:id
+//@access   public
+exports.products = (req, res, next) => {
+  res.render('products');
+};
+
+//@desc     show full details of selected product
+//@route    get  /shop/v1/products/:id
+//@access   public
+exports.item = (req, res, next) => {
+  res.render('item');
+};
+
+//@desc     show all items inside cart
+//@route    get  /shop/v1/card
+//@access   public
+exports.cart = (req, res, next) => {
+  res.render('cart');
 };
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const { index } = require('../controllers/user');
+const { index, item, products, cart } = require('../controllers/user');
 
 const router = express.Router();
 
 router.route('/').get(index);
+router.route('/products/:id').get(item);
+router.route('/category/:id').get(products);
+router.route('/cart').get(cart);
 
 module.exports = router;
